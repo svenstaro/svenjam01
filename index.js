b6fa0a5680be563300164ec229fbf5984bc1fe38
Rafael Epplee <raffomania@gmail.com>
2019-10-18 22:53:53 +0200

    use loader

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ import * as PIXI from 'pixi.js';
 const app = new PIXI.Application({ backgroundColor: 0x1099bb });
 document.body.appendChild(app.view);
 
-const bunny = PIXI.Sprite.from(require('./img/pikachu.png'));
+PIXI.Loader.shared
+    .add('pika', require('./img/pikachu.png'))
+    .load(setup);
 
-bunny.anchor.set(0.5);
+function setup(loader, resources) {
+    const bunny = new PIXI.Sprite(resources.pika.texture);
+    bunny.anchor.set(0.5);
 
-bunny.x = app.screen.width / 2;
-bunny.y = app.screen.height / 2;
+    bunny.x = app.screen.width / 2;
+    bunny.y = app.screen.height / 2;
 
-app.stage.addChild(bunny);
+    app.stage.addChild(bunny);
 
-app.ticker.add((delta) => {
-    bunny.rotation += 0.1 * delta;
-});
+    app.ticker.add((delta) => {
+        bunny.rotation += 0.1 * delta;
+    });
+}
