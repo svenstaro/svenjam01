a1c280967bb6328d5d0ef30c8bbbb5dbde367039
Alexander Dmitriev <alexander.dmitriev2580@gmail.com>
2019-10-19 00:50:58 +0200

    Merge

--- index.js
+++ index.js
@@ -2,12 +2,32 @@ import * as PIXI from 'pixi.js';
 import * as Matter from 'matter-js';
 import update from './src/update.js';
 import Entity from './src/entity.js';
+import tmx from 'tmx-parser';
 
 const app = new PIXI.Application({ backgroundColor: 0x1099bb });
 var engine = Matter.Engine.create();
 const Engine = Matter.Engine
 const World = Matter.World;
 document.body.appendChild(app.view);
+
+// Load static files into app.
+const staticFiles = {
+  "./levels/level1.tmx": require("./levels/level1.tmx"),
+};
+tmx.readFile = (filename, callback) => {
+  console.log(`Trying to log ${filename}`);
+  fetch(staticFiles[filename]).then(response => {
+    const result = response.text().then(text => {
+      callback(undefined, text);
+    });
+  });
+};
+
+tmx.parseFile("./levels/level1.tmx", (err, map) => {
+  if (err) throw err;
+  console.log(map);
+});
+
 
 PIXI.Loader.shared
     .add('pika', require('./img/pikachu.png'))
