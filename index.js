62cd2da3e2c70edc66c66ba78789b263df0f06a7
Rafael Epplee <raffomania@gmail.com>
2019-10-18 22:51:51 +0200

    add basic rendering

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import * as PIXI from 'pixi.js';
 
-let type = "WebGL";
-if(!PIXI.utils.isWebGLSupported()){
-    type = "canvas";
-}
+const app = new PIXI.Application({ backgroundColor: 0x1099bb });
+document.body.appendChild(app.view);
 
-PIXI.utils.sayHello(type);
+const bunny = PIXI.Sprite.from(require('./img/pikachu.png'));
+
+bunny.anchor.set(0.5);
+
+bunny.x = app.screen.width / 2;
+bunny.y = app.screen.height / 2;
+
+app.stage.addChild(bunny);
+
+app.ticker.add((delta) => {
+    bunny.rotation += 0.1 * delta;
+});
