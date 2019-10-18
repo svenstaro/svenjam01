c51d157a6b3d25b4ed19001bbc4ef2babd09607f
Sven-Hendrik Haase <svenstaro@gmail.com>
2019-10-19 00:50:09 +0200

    I don't even

---
+++ levels/character.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="character" tilewidth="32" tileheight="32" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="32" height="32" source="../img/coolerDude.png"/>
+ </tile>
+</tileset>
--- levels/industrial.v2.tsx
+++ levels/industrial.v2.tsx
@@ -1,4 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.5" name="industrial.v2" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
  <image source="../img/industrial.v2.png" width="512" height="512"/>
+ <wangsets>
+  <wangset name="Grass Ground" tile="41">
+   <wangedgecolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="38" wangid="0x1010201"/>
+   <wangtile tileid="39" wangid="0x2010201"/>
+   <wangtile tileid="40" wangid="0x2010201"/>
+   <wangtile tileid="41" wangid="0x2010201"/>
+   <wangtile tileid="42" wangid="0x2010201"/>
+   <wangtile tileid="43" wangid="0x2010101"/>
+  </wangset>
+ </wangsets>
 </tileset>
