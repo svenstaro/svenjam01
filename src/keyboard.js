1044fe3263ecaea0bb0a2d4138af39e20513aa6a
Rafael Epplee <raffomania@gmail.com>
2019-10-19 10:28:24 +0200

    add missing files

---
+++ src/keyboard.js
@@ -0,0 +1,21 @@
+const activeKeys = {};
+
+const keyboard = {
+    init() {
+        window.addEventListener('keydown', (event) => {
+            let key = event.which || event.keyCode;
+            activeKeys[key] = true;
+        });
+
+        window.addEventListener('keyup', (event) => {
+            let key = event.which || event.keyCode;
+            activeKeys[key] = false;
+        });
+    },
+
+    isPressed(key) {
+        return !!activeKeys[key];
+    },
+};
+
+export default keyboard;
