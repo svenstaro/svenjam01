230be10be86934243dbb9ec6733647dff58d6da2
Arne Beer <arne@twobeer.de>
2019-10-19 19:42:39 +0200

    Move level creation into map.js

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 import * as PIXI from 'pixi.js';
 import * as Matter from 'matter-js';
 import level1 from './levels/level1.json';
-import {TILES_MAP} from "./src/tileset_helper";
 import lerp from 'lerp';
-import { setAnimationTilesetTexture } from "./src/animations.js";
-
-import Box from './src/entities/box';
+import {load_level} from './src/map';
 
 // Base stuff
 import {getSpawn, getGameObjects} from './src/map';
 import update from './src/update';
 
 // Physics
-import Body from './src/physics/body';
 import collisions from './src/physics/collisions';
 
 // Entities
@@ -71,150 +67,4 @@ function create_entities(resources) {
     ]);
 
     return new Set([player, ...gameObjects]);
-}
-
-function load_level(level) {
-    app.renderer.backgroundColor = Number.parseInt(level.backgroundcolor.replace('#', '0x'));
-    const atlas = PIXI.BaseTexture.from('tileset');
-    const atlas_width = 32
-    const atlas_height = 32
-
-    // Iterate through each layer
-    for (let j = 0; j < level.layers.length; j++) {
-        let layer = level.layers[j];
-
-        // Ignore the GameObjects layer. We need to handle this individually.
-        if (layer.name === "Game Objects") {
-            continue;
-        }
-        for (let i = 0; i < layer.data.length; i++) {
-            if (layer.data[i] === 0) {
-                continue;
-            }
-            let extracted_data = get_flipping_and_id(layer.data[i]);
-            // Calculate atlas_id (global_id - tileset_id)
-            let atlas_id = extracted_data.global_id - 1;
-
-            // Calculate position in tileset
-            const atlas_row = Math.floor(parseFloat(atlas_id) / parseFloat(atlas_width));
-            const atlas_col = atlas_id % atlas_width
-
-            // Create textures
-            let current_texture = new PIXI.Texture(atlas, new PIXI.Rectangle(atlas_col*16, atlas_row*16, 16, 16));
-            let current_sprite = new PIXI.Sprite(current_texture);
-
-            // Calculation and set positions
-            const canvas_position_x = (i * 16) % (level.width * 16);
-            const canvas_position_y = Math.floor(i / 100) * 16;
-            current_sprite.position.x = canvas_position_x;
-            current_sprite.position.y = canvas_position_y;
-
-            // Add prite
-            app.stage.addChild(current_sprite);
-
-            /// Create static physics objects for foreground tiles
-            if (layer.name === "Foreground Tiles") {
-                create_static_body(atlas_id, canvas_position_x, canvas_position_y);
-            }
-        }
-    }
-
-    add_background_colors(app)
-}
-
-
-function create_static_body(atlas_id, pos_x, pos_y) {
-    //    console.log("Id and map");
-    //    console.log(atlas_id);
-    //    console.log(TILES_MAP);
-    let tile = TILES_MAP[atlas_id];
-    // Ignore this for now. Debug lol
-    if (tile === undefined) {
-        return
-    }
-    // Ignore animation tiles. These are handled elsewhere
-    if (tile.animation !== undefined) {
-        return
-    }
-    let objects = tile.objectgroup.objects;
-
-    //    console.log("tile and objects");
-    //    console.log(tile);
-    //    console.log(objects);
-    // There can be multiple objects per tile (we usually have one)
-    for (const object of objects) {
-        // We got a simple box object
-        if (object.polygon !== undefined) {
-            // Calculate physic body offset to tile
-            let position_x = pos_x + object.x;
-            let position_y = pos_y + object.y;
-
-            let body = Matter.Bodies.rectangle(
-                position_x,
-                position_y,
-                object.width,
-                object.height,
-            {
-                isStatic: true,
-                label: 'ground',
-            });
-            //            console.log("Create body")
-            //            console.log(position_x, position_y)
-            //            console.log(object.height, object.width)
-            World.add(engine.world, [body]);
-
-        } else {
-            // Rectangle for everything else for debugging
-            let body = Matter.Bodies.rectangle(
-                pos_x,
-                pos_y,
-                16,
-                16,
-            {
-                isStatic: true,
-                label: 'ground',
-            });
-            //            console.log("Create body")
-            //            console.log(pos_x, pos_y)
-            //            console.log(object.height, object.width)
-            World.add(engine.world, [body]);
-        }
-    }
-}
-
-function add_background_colors(app) {
-    let first_sprite = new PIXI.Graphics();
-    first_sprite.beginFill(0xFF0000);
-    first_sprite.drawRect(0, 0, 16, 16);
-    first_sprite.endFill();
-
-    app.stage.addChild(first_sprite);
-
-    let last_sprite = new PIXI.Graphics();
-    last_sprite.beginFill(0xFF0000);
-    last_sprite.drawRect(1600, 1600, 16, 16);
-    last_sprite.endFill();
-
-    app.stage.addChild(last_sprite);
-}
-
-function get_flipping_and_id(id) {
-    let horizontal_bit = 0b10000000000000000000000000000000;
-    let vertical_bit = 0b01000000000000000000000000000000;
-    let diagonal_bit = 0b00100000000000000000000000000000;
-
-    let clean_bits = 0b00011111111111111111111111111111;
-
-    let horizontal = (horizontal & id) == horizontal;
-    let vertical = (vertical_bit & id) == vertical_bit;
-    let diagonal = (diagonal_bit & id) == diagonal_bit;
-
-    let normalized_id = clean_bits & id;
-
-    return {
-        horizontal: horizontal,
-        vertical: vertical,
-        diagonal: diagonal,
-        global_id: normalized_id,
-    }
 }
